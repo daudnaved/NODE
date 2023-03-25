8179363b3f40e6d83936a1eec44e08d6056a81e8
daudnaved <fa19-bcs-132@cuilahore.edu.pk>
2023-03-26 00:43:20 +0500

    deleted

--- data.js
+++
@@ -1,6 +0,0 @@
-data = [
-    {Name: "Dawood Naveed", Email: "anybody@cui.edu.pk", Contact: "090078601"},
-    {Name: "Shahnawaz", Email: "shani@cui.edu.pk", Contact: "090078601"},
-    {Name: "Dawood Naveed", Email: "nobody@cui.edu.pk", Contact: "090078601"}
-]
-module.exports = data
