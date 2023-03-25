cefde63253c46dacc109300f7ff9bc62210dd4e2
daudnaved <fa19-bcs-132@cuilahore.edu.pk>
2023-03-26 00:33:04 +0500

    Promise File

---
+++ Promise.js
@@ -0,0 +1,17 @@
+// // console.log('Start execution')
+//     a=10,
+//     b=40
+
+//     watingData = new Promise((resolve,reject)=>{
+//         setTimeout(()=>{
+//             resolve(20)
+//     },3000)
+    
+//     })
+//     watingData.then((data)=>{
+//         b=data
+//         console.log(a+b)
+//     })
+
+    
+
